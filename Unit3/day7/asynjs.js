eab51528bb145db95543c2a95461072827bb6bf1
parag jagdale <paragj28@gmail.com>
2021-12-29 00:19:03 +0530

    masai

---
+++ Unit3/day7/asynjs.js
@@ -0,0 +1,51 @@
+// function abc(s){
+//   let script = document.createElement('script')
+
+//   script.src = s
+//   document.head.append(screen)
+// }
+
+//  setTimeout(()=>{
+//      hello()
+//  },3000)
+
+// abc("/myscript.js")
+
+// const myPromise = new Promise((resolve, reject) => {  
+//     // condition
+//     resolve("i am resolve")
+// });
+
+// myPromise.then((response)=>{
+//     console.log("response:",response)
+// }).catch(function(error){
+//     console.log("error:",error)
+// })
+
+
+//------------------EXAMPLE NO!============
+
+
+//let passexam = false
+
+// let parentPromise = new Promise(function(resolve,reject){
+//     setInterval(() => {
+//         if(passexam == true){
+//             resolve("get bike")
+//         }else {
+//             reject("take a samadhi")
+//         } 
+//     }, 3000);
+   
+// })
+// parentPromise.then((resolve)=>{
+//     console.log("resolve",resolve)
+// }).catch((reject)=>{
+//     console.log("reject:",reject)
+// })
+
+//====================----EXAMPLE2----============//
+
+
+
+
